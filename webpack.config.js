b71ca342034cd340808b119694e90cfe0cec4a0e
Alex Modin <almod90@gmail.com>
2019-10-30 15:22:37 +0400

    scss setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     mode: "development",
     entry: [
@@ -9,8 +11,23 @@ module.exports = {
         filename: 'app.js',
         path: path.resolve(__dirname, 'dist/')
     },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
+        hot: false,
         port: 9000
     }
 };
