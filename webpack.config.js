7b60efffbb45e2139d21d5df4b3ce1390b91f8e0
Alex Modin <almod90@gmail.com>
2019-10-30 15:16:02 +0400

    initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: [
+        './src/index.js'
+    ],
+    output:{
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist/')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000
+    }
+};
